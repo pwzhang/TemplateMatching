31371d87ea680e921d1e3d0662b4dffc3905554a
Powei Zhang <zhangpw@bu.edu>
2017-11-05 22:56:17 -0500

    change picture

--- TemplateMatching.py
+++ TemplateMatching.py
@@ -1,13 +1,15 @@
 import numpy as np
 import cv2
 
-def TemplateMatching(src, temp, stepsize): # src: source image, temp: template image, stepsize: the step size for sliding the template
+
+def TemplateMatching(src, temp,
+                     stepsize):  # src: source image, temp: template image, stepsize: the step size for sliding the template
     mean_t = 0;
     var_t = 0;
     location = [0, 0];
     # Calculate the mean and variance of template pixel values
-    # ------------------ Put your code below ------------------ 
-                    
+    # ------------------ Put your code below ------------------
+
     max_corr = 0;
     # Slide window in source image and find the maximum correlation
     for i in np.arange(0, src.shape[0] - temp.shape[0], stepsize):
@@ -16,28 +18,30 @@ def TemplateMatching(src, temp, stepsize): # src: source image, temp: template i
             var_s = 0;
             corr = 0;
             # Calculate the mean and variance of source image pixel values inside window
-            # ------------------ Put your code below ------------------ 
-            
+            # ------------------ Put your code below ------------------
+
             # Calculate normalized correlation coefficient (NCC) between source and template
-            # ------------------ Put your code below ------------------ 
-            
+            # ------------------ Put your code below ------------------
+
             if corr > max_corr:
                 max_corr = corr;
                 location = [i, j];
     return location
 
+
+print("11111")
 # load source and template images
-source_img = cv2.imread('Source.jpg',0) # read image in grayscale
-temp = cv2.imread('template.jpg',0) # read image in grayscale
+source_img = cv2.imread('Source.jpg', 0)  # read image in grayscale
+temp = cv2.imread('template.jpg', 0)  # read image in grayscale
 location = TemplateMatching(source_img, temp, 20);
 print(location)
 match_img = cv2.cvtColor(source_img, cv2.COLOR_GRAY2RGB)
 
 # Draw a red rectangle on match_img to show the template matching result
-# ------------------ Put your code below ------------------ 
+# ------------------ Put your code below ------------------
 
 # Save the template matching result image (match_img)
-# ------------------ Put your code below ------------------ 
+# ------------------ Put your code below ------------------
 
 
 # Display the template image and the matching result
